2c1322cc057aaf0c3a40cf03a5f7a2f154a28985
장서영 <bnrsy86@gmail.com>
2025-05-05 09:49:03 +0900

    최초 업로드

---
+++ 설치파일/install_pymupdf.bat
@@ -0,0 +1,4 @@
+@echo off
+echo PyMuPDF (fitz) ¼³Ä¡ Áß...
+"C:\Users\bnrsy\AppData\Local\Programs\Python\Python312\python.exe" -m pip install PyMuPDF
+pause
---
+++ 설치파일/install_streamlit.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Streamlit ¼³Ä¡ Áß...
+"C:\Users\bnrsy\AppData\Local\Programs\Python\Python312\python.exe" -m pip install --upgrade pip
+"C:\Users\bnrsy\AppData\Local\Programs\Python\Python312\python.exe" -m pip install streamlit
+pause
