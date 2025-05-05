a96d9d7263d750253037d33dcc682e9590573aec
Seoyoung <naraboxer@naver.com>
2025-05-05 11:20:30 +0900

    Delete 설치파일 directory

--- 설치파일/install_pymupdf.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo PyMuPDF (fitz) ¼³Ä¡ Áß...
-"C:\Users\bnrsy\AppData\Local\Programs\Python\Python312\python.exe" -m pip install PyMuPDF
-pause
--- 설치파일/install_streamlit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Streamlit ¼³Ä¡ Áß...
-"C:\Users\bnrsy\AppData\Local\Programs\Python\Python312\python.exe" -m pip install --upgrade pip
-"C:\Users\bnrsy\AppData\Local\Programs\Python\Python312\python.exe" -m pip install streamlit
-pause
